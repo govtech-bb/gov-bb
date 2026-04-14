9c00ec12654a0154bbef395101824ff1c4648352
IsaiahSama <isaiahcarrington46@gmail.com>
2026-04-14 19:32:48 -0400

    Had to add another component to parse params, because Next is weird. TanStack Router migration soon

--- apps/web/src/app/forms/[id]/page.tsx
+++ apps/web/src/app/forms/[id]/page.tsx
@@ -1,13 +1,14 @@
-import { FormRenderer } from "@web/components";
+import { Form } from "@web/components";
 import { fetchContract } from "@web/lib";
 import { designSystem } from "apps/web/src/lib/design-system";
+import * as React from "react";
 
-export default async function Form({ params }: { params: { id: string } }) {
-  const contract = await fetchContract(params.id); // Doesn't work cause of next JS nonsense
+export default async function Page({ params }: { params: { id: string } }) {
+  // Await is necessary, else it breaks.
+  const { id } = await params;
   return (
     <div className={designSystem.formRoot}>
-      <h1>Form {params.id}</h1>
-      <FormRenderer contract={contract} />
+      <Form id={id} />
     </div>
   );
 }
--- apps/web/src/components/index.ts
+++ apps/web/src/components/index.ts
@@ -1,5 +1,7 @@
 import FormRenderer from "./form-renderer";
 import FieldRenderer from "./field-renderer";
+import Form from "./form";
 
 export { FormRenderer };
 export { FieldRenderer };
+export { Form };
