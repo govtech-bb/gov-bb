1675ff6ab0b02b3325e517d8c05c6d50e3035df0
Tarika Birch <tarikabirch@gmail.com>
2026-04-15 15:13:17 -0400

    Refactored forms/id page  to remove design system wrapper

--- apps/web/src/app/forms/[id]/page.tsx
+++ apps/web/src/app/forms/[id]/page.tsx
@@ -1,14 +1,10 @@
 import { Form } from "@web/components";
-import { fetchContract } from "@web/lib";
-import { designSystem } from "apps/web/src/lib/design-system";
 import * as React from "react";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  // Await is necessary, else it breaks.
-  const { id } = await params;
-  return (
-    <div className={designSystem.formRoot}>
-      <Form id={id} />
-    </div>
-  );
+    // Await is necessary, else it breaks.
+    const { id } = await params;
+    return (
+        <Form id={id} />
+    );
 }
