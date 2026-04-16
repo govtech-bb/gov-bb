4ee5f763d1df146f10ec4dd7fe44c5b94bf15a4d
IsaiahSama <isaiahcarrington46@gmail.com>
2026-04-16 12:03:50 -0400

    process.env is now setup

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,17 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    tsconfigPaths: true,
-  },
-  plugins: [
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
-});
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    define: {
+      'process.env': {
+        ...env
+      }
+    },
+    resolve: {
+      tsconfigPaths: true,
+    },
+    plugins: [
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+      }),
+      react(),
+    ],
+  });
+
+}
