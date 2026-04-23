e9b2afb8415c3c1e54560a781955039a30ad2c2f
IsaiahSama <isaiahcarrington46@gmail.com>
2026-04-23 15:09:29 -0400

    Fixed introduced issue with error box showing when no errors was present

--- apps/web/src/components/error-summary.tsx
+++ apps/web/src/components/error-summary.tsx
@@ -22,6 +22,8 @@ export default function ErrorSummary({
     );
   }
 
+  if (fieldErrorItems.length === 0) return null;
+
   return (
     <div data-error-summary role="alert">
       <h2>There is a problem</h2>
