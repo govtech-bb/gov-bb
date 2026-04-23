265a9ed92f02df4bb50aecb08c148afd31b28c8e
Tarika Birch <tarikabirch@gmail.com>
2026-04-23 09:48:54 -0400

    Updated form renderer to prevent URL skipping through forms

--- apps/web/src/components/form-renderer.tsx
+++ apps/web/src/components/form-renderer.tsx
@@ -4,6 +4,11 @@ import FieldRenderer from "./field-renderer";
 import designSystem from "../lib/design-system";
 import React, { useEffect } from "react";
 import ErrorSummary from "./error-summary";
+import {
+  getLastCompletedStep,
+  isStepCompleted,
+  markStepCompleted,
+} from "../lib/session-storage";
 
 export default function FormRenderer({
   form,
@@ -14,28 +19,11 @@ export default function FormRenderer({
   const [hidePrevious, setHidePrevious] = React.useState(true);
   const navigate = useNavigate({ from: "/forms/$formId/" });
 
-  useEffect(() => {
-    if (stepId) {
-      const requestedStepIndex = formMeta.steps.findIndex((formStep) => {
-        return formStep.stepId === stepId;
-      });
-
-      setStepIndex(requestedStepIndex >= 0 ? requestedStepIndex : 0);
-      return;
-    }
-
-    setStepIndex(0);
-  }, [stepId, formMeta.steps]);
-
-  useEffect(() => {
-    if (stepIndex === 0) {
-      setHidePrevious(true);
-    } else {
-      setHidePrevious(false);
-    }
-  }, [stepIndex]);
-
-  const currentStep = formMeta.steps[stepIndex];
+  const findStepIndexbyId = (stepId: string) => {
+    return formMeta.steps.findIndex((formStep) => {
+      return formStep.stepId === stepId;
+    });
+  };
 
   const navigateToStep = (nextStepIndex: number) => {
     if (nextStepIndex < 0 || nextStepIndex >= formMeta.steps.length) {
@@ -53,16 +41,66 @@ export default function FormRenderer({
     });
   };
 
+  useEffect(() => {
+    if (stepId) {
+      const requestedStepIndex = findStepIndexbyId(stepId);
+
+      setStepIndex(requestedStepIndex >= 0 ? requestedStepIndex : 0);
+
+      if (!isStepCompleted(form._formId, stepId)) {
+        const previousStepIndex = requestedStepIndex - 1;
+
+        if (
+          previousStepIndex >= 0 &&
+          !isStepCompleted(
+            form._formId,
+            formMeta.steps[previousStepIndex].stepId,
+          )
+        ) {
+          const lastCompletedStepId = getLastCompletedStep(
+            form._formId,
+            formMeta.steps,
+          );
+          const lastCompletedStepIndex = lastCompletedStepId
+            ? findStepIndexbyId(lastCompletedStepId)
+            : -1;
+          setStepIndex(
+            lastCompletedStepIndex >= 0 ? lastCompletedStepIndex : 0,
+          );
+          navigateToStep(
+            lastCompletedStepIndex >= 0 ? lastCompletedStepIndex : 0,
+          );
+          return;
+        }
+      }
+
+      return;
+    }
+
+    setStepIndex(0);
+  }, [stepId, formMeta.steps]);
+
+  useEffect(() => {
+    if (stepIndex === 0) {
+      setHidePrevious(true);
+    } else {
+      setHidePrevious(false);
+    }
+  }, [stepIndex]);
+
+  const currentStep = formMeta.steps[stepIndex];
+
   const handlePrevious = () => {
     navigateToStep(stepIndex - 1);
   };
 
   const handleContinue = () => {
+    // TODO: Validate current step before marking as completed and navigating to the next step
+    markStepCompleted(form._formId, currentStep.stepId);
     navigateToStep(stepIndex + 1);
   };
 
-  const handleSubmit = () => {
-  };
+  const handleSubmit = () => {};
 
   return (
     <div className={designSystem.formRoot}>
@@ -75,12 +113,21 @@ export default function FormRenderer({
 
       <div className={designSystem.formStep}>
         {currentStep.fields.map((field) => (
-          <FieldRenderer key={field.id} form={form} field={field} validationProperties={formMeta.validationProperties[field.id]} />
+          <FieldRenderer
+            key={field.id}
+            form={form}
+            field={field}
+            validationProperties={formMeta.validationProperties[field.id]}
+          />
         ))}
 
         <div className={designSystem.formNavigation}>
           {!hidePrevious && (
-            <button data-variant="secondary" type="button" onClick={handlePrevious}>
+            <button
+              data-variant="secondary"
+              type="button"
+              onClick={handlePrevious}
+            >
               Previous
             </button>
           )}
