4a8f25539c87de8b9fde9e8d3028b173e09d4c6e
= <=>
2026-04-27 16:25:55 +0500

    refactor(types): remove deprecated type definitions and schemas from form-types package

--- packages/form-types/src/block.type.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Primitive } from "./primitive.type";
-import type { Behaviour } from "./behavior.type";
-export interface Block {
-  blockId: string;
-  blockDescription: string;
-  blockVersion: string;
-  elements: Array<Primitive>;
-  behaviours?: Array<Behaviour>;
-}
--- packages/form-validation/src/validate-field.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Primitive } from "@govtech-bb/form-types";
-import type { StepScopedValues } from "./types";
-export declare function validateField(
-  field: Primitive,
-  value: unknown,
-  allValues: StepScopedValues,
-  stepValues?: Record<string, unknown>,
-): string[];
