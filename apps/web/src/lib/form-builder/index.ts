3c0b6661cb38e935cdae2062be8ffda8a413f94f
IsaiahSama <isaiahcarrington46@gmail.com>
2026-04-30 12:19:55 -0400

    Moved buildForm to its own file

--- apps/web/src/lib/form-builder/index.ts
+++ apps/web/src/lib/form-builder/index.ts
@@ -1,31 +1,4 @@
-// Responsible for turning a ClientServiceContract into a FormMeta for consumption.
-
-import { ClientServiceContract, FormMeta, FormValidation } from "@web/types";
-import { buildValidation } from "./validation-builder";
-import { getStepConditonalTargets } from "./behavior-helper";
-
-export const buildForm = (contract: ClientServiceContract): FormMeta => {
-  // Build the Validation Schema
-  const { schema, defaults, properties }: FormValidation =
-    buildValidation(contract);
-
-  const stepConditionalTargets = getStepConditonalTargets(contract.steps);
-
-  // Return FormMeta object with everything configured.
-  return {
-    formId: contract.formId,
-    formTitle: contract.title,
-    formDescription: contract.description,
-    schema,
-    steps: contract.steps,
-    defaultValues: defaults,
-    validationProperties: properties,
-    stepConditionalTargets,
-  };
-};
-
-// Other Exports
-
 export { checkConditionalOn, getVisibleSteps } from "./behavior-helper";
 export type { RequiredState } from "./validation-methods";
 export { fetchContract, FormFetchError } from "./form-fetcher";
+export { buildForm } from "./build-form";
