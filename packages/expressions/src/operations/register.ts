d75f960dc0824ea8abc7ff7cc45d5010b6276087
Akinola Raphael <rakinola90@gmail.com>
2026-05-07 15:30:19 +0100

    feat(operations): add register function to initialize custom jsonLogic operations

---
+++ packages/expressions/src/operations/register.ts
@@ -0,0 +1,12 @@
+import type jsonLogicType from "json-logic-js";
+import { age } from "./age";
+import { today } from "./today";
+import { daysBetween } from "./days-between";
+import { currency } from "./currency";
+
+export function registerOperations(jsonLogic: typeof jsonLogicType): void {
+  jsonLogic.add_operation("age", age);
+  jsonLogic.add_operation("today", today);
+  jsonLogic.add_operation("daysBetween", daysBetween);
+  jsonLogic.add_operation("currency", currency);
+}
