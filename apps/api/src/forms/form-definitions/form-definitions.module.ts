fff2cef2588328ded33b5cf4febe4c0e5eece5b4
IsaiahSama <zeldaplayergl@gmail.com>
2026-05-22 13:51:59 -0400

    feat(api): register DraftArchiveController/Service in FormDefinitionsModule

--- apps/api/src/forms/form-definitions/form-definitions.module.ts
+++ apps/api/src/forms/form-definitions/form-definitions.module.ts
@@ -3,16 +3,19 @@ import { FormDefinitionsController } from "./form-definitions.controller";
 import { FormDefinitionsService } from "./form-definitions.service";
 import { FormDefinitionRepository } from "./form-definition.repository";
 import { RecipeFileLoaderService } from "./recipe-file-loader.service";
+import { DraftArchiveController } from "./draft-archive.controller";
+import { DraftArchiveService } from "./draft-archive.service";
 import { FormDisabledOverridesModule } from "../form-disabled-overrides/form-disabled-overrides.module";
 import { RegistryModule } from "../../registry/registry.module";
 
 @Module({
   imports: [RegistryModule, FormDisabledOverridesModule],
-  controllers: [FormDefinitionsController],
+  controllers: [FormDefinitionsController, DraftArchiveController],
   providers: [
     FormDefinitionsService,
     FormDefinitionRepository,
     RecipeFileLoaderService,
+    DraftArchiveService,
   ],
   exports: [FormDefinitionsService],
 })
