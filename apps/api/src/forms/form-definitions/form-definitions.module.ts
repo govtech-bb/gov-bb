279e91ce90fab94fd60b67727a0357960d672ac5
IsaiahSama <zeldaplayergl@gmail.com>
2026-05-22 12:27:48 -0400

    feat(api): short-circuit GET /form-definitions/:formId on disabled override

--- apps/api/src/forms/form-definitions/form-definitions.module.ts
+++ apps/api/src/forms/form-definitions/form-definitions.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { FormDefinitionsController } from "./form-definitions.controller";
 import { FormDefinitionsService } from "./form-definitions.service";
 import { FormDefinitionRepository } from "./form-definition.repository";
+import { FormDisabledOverridesModule } from "../form-disabled-overrides/form-disabled-overrides.module";
 import { RegistryModule } from "../../registry/registry.module";
 
 @Module({
-  imports: [RegistryModule],
+  imports: [RegistryModule, FormDisabledOverridesModule],
   controllers: [FormDefinitionsController],
   providers: [FormDefinitionsService, FormDefinitionRepository],
   exports: [FormDefinitionsService],
